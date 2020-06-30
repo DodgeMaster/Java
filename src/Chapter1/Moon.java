690845e540b57595f7297e98908728f5d2e91eba
= <=>
2020-06-30 11:24:23 +0300

    Initial commit

---
+++ src/Chapter1/Moon.java
@@ -0,0 +1,13 @@
+package Chapter1;
+
+public class Moon {
+    public static void main(String[] args) {
+        double  moonWeight, earthWeight;
+
+        earthWeight = 82.6;
+        moonWeight = 0.17 * earthWeight;
+
+        System.out.println(earthWeight +" is earth weight " + moonWeight +
+                " is moon weight.");
+    }
+}
